8730d1eedb689beef87dc0907352c6be9cc5c507
Jason Schorr <echohtp@gmail.com>
2022-04-27 02:10:15 -0400

    list back and forth thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,149 +4,147 @@ import Image from 'next/image'
 import { Navbar } from '../components/navbar'
 import { useMemo, useState } from 'react'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import {
-  LAMPORTS_PER_SOL,
-  Transaction,
-  PublicKey,
-} from '@solana/web3.js'
+import { LAMPORTS_PER_SOL, Transaction, PublicKey } from '@solana/web3.js'
 import { gql, useQuery } from '@apollo/client'
 import client from '../client'
-import { createAssociatedTokenAccountInstruction, getAssociatedTokenAddress, getOrCreateAssociatedTokenAccount, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, createTransferInstruction } from "@solana/spl-token";
-
+import {
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+  getOrCreateAssociatedTokenAccount,
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  createTransferInstruction
+} from '@solana/spl-token'
 
 const Home: NextPage = () => {
-const { publicKey, signTransaction } = useWallet()
-const { connection } = useConnection()
+  const { publicKey, signTransaction } = useWallet()
+  const { connection } = useConnection()
 
-const massSend = async (list: Nft[], to: String) => {
-  if (!list || !to || !connection|| !publicKey || !signTransaction) {
-    console.log('returning')
-     return
-  }
+  const massSend = async (list: Nft[], to: String) => {
+    if (!list || !to || !connection || !publicKey || !signTransaction) {
+      console.log('returning')
+      return
+    }
 
-  try { 
-    new PublicKey(to)
-    console.log("valid dest address: ", to)
-  }catch(e: any){
-    console.log("bad dest address")
-    return
-  }
+    try {
+      new PublicKey(to)
+      console.log('valid dest address: ', to)
+    } catch (e) {
+      console.log('bad dest address')
+      return
+    }
 
-  if (!list.length){
-    console.log("probably want to select some nfts")
-    return
-  }
+    if (!list.length) {
+      console.log('probably want to select some nfts')
+      return
+    }
 
-  const tx = new Transaction()
-  for (var i = 0; i < list.length; i++){
-  
-    const mintPublicKey = new PublicKey(list[i].mintAddress); 
-    const fromTokenAccount = await getAssociatedTokenAddress(mintPublicKey, publicKey)
-    const fromPublicKey = publicKey
-    const destPublicKey = new PublicKey(to);
-    const destTokenAccount = await getAssociatedTokenAddress(mintPublicKey, destPublicKey)
-    const receiverAccount = await connection.getAccountInfo(destTokenAccount)
-    
-    if (receiverAccount === null) {
+    const tx = new Transaction()
+    for (var i = 0; i < list.length; i++) {
+      const mintPublicKey = new PublicKey(list[i].mintAddress)
+      const fromTokenAccount = await getAssociatedTokenAddress(
+        mintPublicKey,
+        publicKey
+      )
+      const fromPublicKey = publicKey
+      const destPublicKey = new PublicKey(to)
+      const destTokenAccount = await getAssociatedTokenAddress(
+        mintPublicKey,
+        destPublicKey
+      )
+      const receiverAccount = await connection.getAccountInfo(destTokenAccount)
+
+      if (receiverAccount === null) {
+        tx.add(
+          createAssociatedTokenAccountInstruction(
+            fromPublicKey,
+            destTokenAccount,
+            destPublicKey,
+            mintPublicKey,
+            TOKEN_PROGRAM_ID,
+            ASSOCIATED_TOKEN_PROGRAM_ID
+          )
+        )
+      }
+
       tx.add(
-        createAssociatedTokenAccountInstruction(
-          fromPublicKey,
+        createTransferInstruction(
+          fromTokenAccount,
           destTokenAccount,
-          destPublicKey,
-          mintPublicKey,
-          TOKEN_PROGRAM_ID,
-          ASSOCIATED_TOKEN_PROGRAM_ID
+          fromPublicKey,
+          1
         )
       )
     }
+    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+    tx.feePayer = publicKey
 
-    tx.add(
-      createTransferInstruction(
-        fromTokenAccount,
-        destTokenAccount,
-        fromPublicKey,
-        1
-      )
-    )
+    let signed: Transaction | undefined = undefined
+
+    try {
+      signed = await signTransaction(tx)
+    } catch (e) {
+      console.log(e.message)
+      return
+    }
+
+    let signature: string | undefined = undefined
+
+    try {
+      signature = await connection.sendRawTransaction(signed.serialize())
+
+      await connection.confirmTransaction(signature, 'confirmed')
+    } catch (e) {
+      console.log(e.message)
+    }
   }
-  tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
-  tx.feePayer = publicKey
 
-  let signed: Transaction | undefined = undefined
-
-  try {
-    signed = await signTransaction(tx)
-  } catch (e: any) {
-    console.log(e.message)
-    return
-  }
-
-  let signature: string | undefined = undefined
-
-  try {
-    signature = await connection.sendRawTransaction(signed.serialize())
-
-    await connection.confirmTransaction(signature, 'confirmed')
-  }catch (e: any){
-    console.log(e.message)
-  }
-}
-
-const GET_NFTS = gql`
-   query GetNfts(
-    $owners: [PublicKey!]
-    $limit: Int!
-    $offset: Int!
-   ){
-     nfts(
-      owners: $owners
-      limit: $limit
-      offset: $offset
-     ){
-      address
-      mintAddress
-      name
-      description
-      image
-      owner {
+  const GET_NFTS = gql`
+    query GetNfts($owners: [PublicKey!], $limit: Int!, $offset: Int!) {
+      nfts(owners: $owners, limit: $limit, offset: $offset) {
         address
-        associatedTokenAccountAddress
-      }
-     }
-   }
-`
-
-interface Nft { 
-  name: string
-  address: string
-  description: string
-  image: string
-  mintAddress: string
-}
-
-const [nfts, setNfts] = useState<Nft[]>([])
-const [sending, setSending] = useState<Nft[]>([])
-const [to, setTo] = useState("")
-
-  useMemo(()=>{
-    if (publicKey?.toBase58()){
-      client.query({
-        query: GET_NFTS,
-        variables: {
-          owners: [publicKey?.toBase58()],
-          offset: 0,
-          limit: 200,
+        mintAddress
+        name
+        description
+        image
+        owner {
+          address
+          associatedTokenAccountAddress
         }
-        
-      })
-      .then(res => setNfts(res.data.nfts))
-    }else{
+      }
+    }
+  `
+
+  interface Nft {
+    name: string
+    address: string
+    description: string
+    image: string
+    mintAddress: string
+  }
+
+  const [nfts, setNfts] = useState<Nft[]>([])
+  const [sending, setSending] = useState<Nft[]>([])
+  const [to, setTo] = useState('')
+
+  useMemo(() => {
+    if (publicKey?.toBase58()) {
+      client
+        .query({
+          query: GET_NFTS,
+          variables: {
+            owners: [publicKey?.toBase58()],
+            offset: 0,
+            limit: 200
+          }
+        })
+        .then(res => setNfts(res.data.nfts))
+    } else {
       setNfts([])
     }
-  },[publicKey?.toBase58()])
+  }, [publicKey?.toBase58()])
 
   return (
-    
     <div>
       <Head>
         <title>Create Next App</title>
@@ -157,22 +155,68 @@ const [to, setTo] = useState("")
       <Navbar />
 
       <div className='container'>
-        
         <h1>Connected to: {publicKey?.toBase58()}</h1>
         <div className='grid grid-cols-2 gap-2'>
           <div>
-          <ul>
-          {nfts.map((e)=><li className="flex" style={{"border": "solid 1px black", "marginTop": "10px"}}><img src={e.image} width={"50px"}/><span className='block'>{e.name}</span><button className="block" onClick={()=>setSending([...sending, e])} className='ml-10 text-white bg-blue-600'>Send it</button></li>)}
-        </ul>
+            <ul>
+              {nfts.map(e => (
+                <li
+                  className='flex mx-4 my-2'
+                  style={{ border: 'solid 1px black', marginTop: '10px' }}
+                >
+                  <img src={e.image} width={'50px'} />
+                  <span className='block'>{e.name}</span>
+                  <button
+                    onClick={() => {
+                      setSending([...sending, e])
+                      setNfts(nfts.filter((item)=> item !== e))
+                    }}
+                    className='block ml-10 text-white bg-blue-600'
+                  >
+                    Send it
+                  </button>
+                </li>
+              ))}
+            </ul>
           </div>
           <div>
             <h2>To send</h2>
-            <input type="text" className="" placeholder='pubkey address' onChange={(e)=>{setTo(e.target.value)}}/>
-            <ul>{sending.map((e)=><li>{e.name}</li>)}</ul>
-            <button onClick={()=>massSend(sending, to)} className="border border-black">Send them</button>
+            <input
+              type='text'
+              className=''
+              placeholder='pubkey address'
+              onChange={e => {
+                setTo(e.target.value)
+              }}
+            />
+            <ul>
+              {sending.map(e => (
+                <li
+                className='flex mx-4 my-2'
+                style={{ border: 'solid 1px black', marginTop: '10px' }}
+              >
+                <img src={e.image} width={'50px'} />
+                <span className='block'>{e.name}</span>
+                <button
+                  onClick={() => {
+                    setSending(sending.filter((item)=>item !== e))
+                    setNfts([...nfts, e])
+                  }}
+                  className='block ml-10 text-white bg-blue-600'
+                >
+                  x
+                </button>
+              </li>
+              ))}
+            </ul>
+            <button
+              onClick={() => massSend(sending, to)}
+              className='border border-black'
+            >
+              Send them
+            </button>
           </div>
         </div>
-        
       </div>
 
       <footer></footer>
