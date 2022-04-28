27db56cba3fa0ff10f5f976d47068ecc0c819c40
Jason Schorr <echohtp@gmail.com>
2022-04-28 00:13:42 -0400

    smol fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
   sending: number
 }
 
-export const Navbar = (props: aNavbarPropsny) => {
+export const Navbar = (props: NavbarProps) => {
   const [active, setActive] = useState(false);
   const {connected} = useWallet()
 
@@ -29,16 +29,16 @@ export const Navbar = (props: aNavbarPropsny) => {
               <path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
             </svg>
             <span className='text-xl font-bold tracking-wide uppercase'>
-              Something
+              NFT Multi Send
             </span>
           </a>
         </Link>
-        <Link href='#'>
+        {/* <Link href='#'>
             <button className='font-bold tracking-wide uppercase'>
               Link1
             </button>
           
-        </Link>
+        </Link> */}
         <button
           className='inline-flex p-3 ml-auto rounded outline-none lg:hidden'
           onClick={handleClick}
